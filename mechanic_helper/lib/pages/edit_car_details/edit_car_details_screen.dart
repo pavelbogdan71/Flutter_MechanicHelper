e200c7535e8d72ff3ca8c4d129aceae7b289be9f
Bogdan Pavel <46486442+pavelbogdan71@users.noreply.github.com>
2022-02-09 16:48:14 +0200

    add dropdown for car brands in edit car details

--- mechanic_helper/lib/pages/edit_car_details/edit_car_details_screen.dart
+++ mechanic_helper/lib/pages/edit_car_details/edit_car_details_screen.dart
@@ -1,12 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:mechanic_helper/external_libs/open_container.dart';
-import 'package:mechanic_helper/pages/car_details/car_details_screen.dart';
+import 'package:dropdown_search/dropdown_search.dart';
+import 'package:mechanic_helper/pages/services/database_service.dart';
 
-class EditCarDetailsScreen extends StatelessWidget{
+class EditCarDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text("asdasd");
+    return Container(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Icon(Icons.arrow_back)),
+            ],
+          ),
+          SizedBox(height: 10),
+          DropdownSearch<String>(
+              mode: Mode.MENU,
+              showSelectedItems: true,
+              items: ['asd', 'gdf'],
+              label: "Menu mode",
+              hint: "country in menu mode",
+              popupItemDisabled: (String s) => s.startsWith('I'),
+              onChanged: print,
+              selectedItem: "Brazil"),
+        ],
+      ),
+    );
   }
-
 }
--- mechanic_helper/lib/pages/services/database_service.dart
+++ mechanic_helper/lib/pages/services/database_service.dart
@@ -2,15 +2,16 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mechanic_helper/models/car_details_model.dart';
 
-class DatabaseService{
+class DatabaseService {
+  CollectionReference carDetails =
+      FirebaseFirestore.instance.collection('car_details');
+  CollectionReference carBrands = FirebaseFirestore.instance.collection('cars');
 
-  CollectionReference carDetails = FirebaseFirestore.instance.collection('car_details');
-
-
-
-  Future<void> addCarDetails(CarDetailsModel carDetailsModel){
-    return carDetails
-    .doc('${FirebaseAuth.instance.currentUser.email}')
-        .set(CarDetailsModel.carDetailsToMap(carDetailsModel),SetOptions(merge:true));
+  Future<void> addCarDetails(CarDetailsModel carDetailsModel) {
+    return carDetails.doc('${FirebaseAuth.instance.currentUser.email}').set(
+        CarDetailsModel.carDetailsToMap(carDetailsModel),
+        SetOptions(merge: true));
   }
+
+ 
 }
